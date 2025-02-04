60f9c1cbc37be8520fe2fc71ebaf37499a2d7535
Josseph-my <samuelcordoba999@gmail.com>
2025-02-04 13:13:49 -0600

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaClase
+{
+    class Persona
+    {
+        private string varCedula { get; set; }
+        private string varNombre { get; set; }
+        private int varEdad { get; set; }
+        private string varApellido { get; set; }
+
+        // constructor por parametros
+
+        public Persona(string pCedula, string pNombre, int pEdad, string pApellido)
+        {
+            this.varCedula = pCedula;
+            this.varApellido = pApellido;
+            this.varEdad = pEdad;
+            this.varNombre = pNombre;
+            Console.WriteLine("Se creo una Persona Por Parametros");
+        }
+        // construcotr sin parametros 
+        public Persona()
+        {
+            this.varApellido = "";
+            this.varCedula = "";
+            this.varEdad = 0;
+            this.varNombre = "";
+            Console.WriteLine("se creo una Persona sin parametros ");
+
+
+        }
+
+        public void mostrar()
+        {
+            Console.WriteLine("+++++++ Datos de la Perosna +++++++");
+            Console.WriteLine($"        Nombre: { this.varNombre} ");
+            Console.WriteLine($"        Apellidos: {this.varApellido}");
+            Console.WriteLine($"        Edad: {this.varEdad}");
+            Console.WriteLine($"        Cedula: {this.varCedula}");
+        }
+        public string resturnoStr()
+        {
+            return "";
+        }
+
+        public int returnoInt()
+        {
+            return 0;
+
+        }
+
+        public bool returnoBool()
+        {
+           return true ;
+        }
+        public float returnoFloat()
+        {
+            return 0;
+        }
+
+        public Double returnoDouble()
+        {
+            return 0;
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+   
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonaClase;
+
+namespace Semana02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo");
+            Persona p1 = new Persona();
+            Persona p2 = new Persona("604740594", "Jorge", 22, "Zamora");
+            p2.mostrar();
+        }
+    }
+}
